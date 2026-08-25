f6eceb32a2c9b5df24e3ebce17a0b374ce770560
kohlantaculturefoot62-cell <kohlantaculturefoot62@gmail.com>
2026-08-25 15:53:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ui import View, Select, Button
 import io
 
 # --- CONFIGURATION ---
-TOKEN = "TON_TOKEN_DISCORD_ICI"
+TOKEN = "MTU0MTg5ODAzNjE2MTAyMDA1NQ.G_nY28.K2LUJwNdvnm9SRp1h86hTUjtAv31XELMBLdlLY"
 ADMIN_LOG_CHANNEL_ID = 123456789012345678  # ID de ton salon admin
 CATEGORY_ID = None  # (Optionnel) ID de la catégorie des salons
 
